02a7d32aea840cee16c93a5b9c9f5ede3626b28f
Yelnar <yeljnar@gmail.com>
2019-11-05 00:02:31 +0400

    Add dropdown value listener

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -143,6 +143,9 @@ class Dropdown extends HTMLElement {
       $option.addEventListener("click", () => {
         this.option = key
         this.toggleOpen()
+
+        this.dispatchEvent(new CustomEvent("onChange", { detail: key }))
+
         this.render()
       })
 
